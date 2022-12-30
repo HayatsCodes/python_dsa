35d06b392e152f2eb05455213a1d307fe43199ce
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 21:03:53 +0100

    Improving performance

--- filter_func.py
+++ filter_func.py
@@ -1,4 +1,6 @@
 def check_odd(item):
     return item % 2 != 0
 
-new_list = list(filter(check_odd, ))
+my_list =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_list = list(filter(check_odd, my_list))
+print(new_list)
