0ac46ba033b709cfcf9ac586b817df159ddd173e
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 21:44:55 +0100

    Refactoring the code

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ print(list(map(lambda item: item * 2, my_list)))
 new_list = [5, 4, 3]
 print(list(map(lambda item: item**2, new_list)))
 
-a = [(0, 2), (4, 3), (9, 9) (10, -1)]
+a = [(0, 2), (4, 3), (9, 9), (10, -1)]
 
