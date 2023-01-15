f78809e3847820e7fd43d0e3bbae9e1d66f037f4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-15 09:59:51 +0100

    done

--- python_refresher/2-classes.py
+++ python_refresher/2-classes.py
@@ -14,3 +14,41 @@ bob = Person('Bob', 32)
 print(bob.getName())
 bob.birthday()
 print(bob.person_age)
+
+
+
+
+
+
+"""
+Exercise
+
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+You can return the answer in any order.
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
+
+
+
+nums = [3,3]
+target = 6
+
+my_sum = Solution()
+index_list = my_sum.twoSum(nums, target)
+print(index_list)
