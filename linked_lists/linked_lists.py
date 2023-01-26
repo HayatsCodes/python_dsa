d3365a98368bc4bb6dad9adbec0febed14e739b7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-26 17:53:02 +0100

    linked

---
+++ linked_lists/linked_lists.py
@@ -0,0 +1,10 @@
+# Linked lists:
+# They are simillar to lists but stores data and 
+# reference to another element
+
+# Node:
+# The element of a linked list.
+# in a singly linked list there are 2 different fields:
+# 1. Data - contains the value to be stored in the node
+# 2. Next - contains a reference to the next node on the list.
+
