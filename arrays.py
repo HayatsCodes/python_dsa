e90f5e97a5cde1ccb66fe1d9f847d157578f3fb7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-23 22:13:19 +0100

    Refactoring the code

--- arrays.py
+++ arrays.py
@@ -1,4 +1,4 @@
 # static arrays are fixed size
 # reading and writing values to an array is O(1)
 # writing to the last spot of an array is O(1) if available
-# writing to any other spot in the array is inefficient
+# writing to any other spot in the array is inefficient and it's O(n)
