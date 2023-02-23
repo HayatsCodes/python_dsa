7d3da379d80ed80a5e87671ab4d1d7b7a0ce4176
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-23 22:19:15 +0100

    Improving performance

--- arrays.py
+++ arrays.py
@@ -3,6 +3,8 @@
 # removing/writing to the end of an array is O(1)
 # removing/inserting to any other spot in the array is inefficient and it's O(n)
 
+# EXAMPLE:
+
 # Insert n into arr at the next open position.
 # Length is the number of 'real' values in arr, and capacity
 # is the size (aka memory allocated for the fixed size array).
