7dd7c02a592e03ce14dfc7acc961f42e90d16e28
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-23 21:09:22 +0100

    Updating the documentation

---
+++ algo_intro.py
@@ -0,0 +1,22 @@
+# Algorithm
+# It is a set of steps that solve a problem
+
+# Algorithm guidelines:
+# 1. It must have a clearly defined problem statement
+# 2. Algorithm definition must contain a specific set of instructions in a particular order.
+# 3. Each steps of the algorithm should be simplified
+# 4. It should produce a result
+# 5. It should complete and cannot take infinite amount of time.
+
+# Big O:
+# This is a notation that is used to describe the efficiency of an algorithm
+# The common notations are:
+# Polynomial runtimes O(n^k):
+    # O(1) - constant time: it always takes the same time to execute the algorithm.
+    # O(log n) - logarithmic time: it takes the log of n + 1 to executethe algorithm.
+    # O(n) - linear time: it takes the exact amount of input to execute the program
+    # O(n^2) - quadratic time: it takes the square of the input to complete the algorithm
+    # O(n log n) - Quasilinear time
+# Exponential runtimes O(x^n):
+    # Brute force algorithm
+    # factorial/comninatprial
