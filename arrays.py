02ecf9b5501dba41fdc4515c289c3790f4a88417
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-23 22:15:04 +0100

    Updating the documentation

--- arrays.py
+++ arrays.py
@@ -1,4 +1,4 @@
 # static arrays are fixed size
 # reading and writing values to an array is O(1)
-# writing to the last spot of an array is O(1) if available
+# writing to the endof an array is O(1) if available
 # writing to any other spot in the array is inefficient and it's O(n)
