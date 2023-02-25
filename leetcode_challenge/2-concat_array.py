b001e0e6e8770543a13e9bb8b78d8e67c09a7d0d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-25 10:51:13 +0100

    Improving performance

--- leetcode_challenge/2-concat_array.py
+++ leetcode_challenge/2-concat_array.py
@@ -4,3 +4,19 @@
 # ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 # Specifically, ans is the concatenation of two nums arrays.
 # Return the array ans.
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # create an array ans of length 2 * nums.length
+        n = len(nums)
+        ans = [0] * (2 * n)
+        # copy nums value to ans twice
+        for index in range(0, len(nums)):
+            ans[index] = nums[index]
+            ans[index + n] = nums[index]
+        # return ans
+        return ans
